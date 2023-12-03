102d326171ebe06cabbb1afc4a127b031c172d5d
DionnerFigueras <dionnerfigueras@gmail.com>
2023-12-03 15:39:03 -0400

    ultimo update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,24 @@
 <x-layouts.vistaPrincipal titulo="Inicio" meta-description="Inicio meta description">
 
-    <h1>Inicio</h1>
+    <div class="container bg-white w-75 " style="height: 350px">
+        <h1 class="row bg-primary mt-5" style="text-align: center; color: white">
+            <p class="fw-bold">Inicio</p>
+        </h1>
+
+        <h2 style="text-align: center">
+            <p>
+                Sistema de inventario de insumos y medicamentos de la parroquia San Buenaventura. <br>
+
+            </p>
+        </h2>
+
+        <h5 style="text-align: center; margin-top: 150px">
+            <p>
+                Desarrollo por estudiantes de la uneg como parte del servicio comunitario.
+            </p>
+        </h5>
+    </div>
+
 
 
 
